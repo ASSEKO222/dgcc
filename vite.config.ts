915dbeb4692fcf238c243c296bc5d2fe0a05b039
asseko <assekomveazareelmaruis@gmail.com>
2025-06-30 10:58:30 +0100

    deuxieme commit 3

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  },
+   preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['dgcc-1.onrender.com']
   }
 });
